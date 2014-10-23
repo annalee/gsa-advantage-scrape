dc1d300e9fad4c2f7224a3e21fd0558af78d03cf
Aidan Feldman <aidan.feldman@gsa.gov>
2014-10-23 13:53:22 -0400

    have flask app handle cart scraping

--- src/server.py
+++ src/server.py
@@ -1,9 +1,18 @@
 from flask import Flask
+from flask import request
+from flask import jsonify
+import gsa_advantage
+
 app = Flask(__name__)
+# TODO disable in prod
+app.debug = True
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+@app.route('/carts/<cart_id>')
+def hello(cart_id):
+    gsa_adv_u = request.args.get('u')
+    gsa_adv_p = request.args.get('p')
+    response = gsa_advantage.getCart(gsa_adv_u,gsa_adv_p,cart_id)
+    return jsonify(response)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
